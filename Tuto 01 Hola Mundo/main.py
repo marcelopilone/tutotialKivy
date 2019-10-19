f75e3a8b106bd15606b7e8fdbfc70b0012ebf174
marcelopilone <mdpilone@gmail.com>
2019-10-19 15:28:42 -0300

    cosas

--- Tuto 01 Hola Mundo/main.py
+++ Tuto 01 Hola Mundo/main.py
@@ -3,16 +3,45 @@ kivy.require('1.9.0')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.lang import Builder
+
 
 from kivy.config import Config
-Config.set('graphics', 'width', 400)
-Config.set('graphics', 'height', 200)
+Config.set('graphics', 'width', 800)
+Config.set('graphics', 'height', 400)
 
 class Contenedor_01(BoxLayout):
+		def mostrarBoton(self,nroBoton):
+			if nroBoton == 1:
+				self.add_widget(Contenedor_MostrarTexto_Boton_Uno())
+			if nroBoton == 2:
+				self.add_widget(Contenedor_MostrarTexto_Boton_Dos())
+			if nroBoton == 3:
+				self.add_widget(Contenedor_MostrarTexto_Boton_Tres())
+			if nroBoton == 4:
+				self.add_widget(Contenedor_MostrarTexto_Boton_Cuatro())
+
+
+class Titulo(BoxLayout):
 	None
+
+class Contenedor_MostrarTexto_Boton_Uno(GridLayout):
+	None
+
+class Contenedor_MostrarTexto_Boton_Dos(BoxLayout):
+	None
+
+class Contenedor_MostrarTexto_Boton_Tres(BoxLayout):
+	None
+
+class Contenedor_MostrarTexto_Boton_Cuatro(BoxLayout):
+	None	
 	
-class MainApp(App):
+class MainApp(App):	
 	title = "Hola Mundo"
+
 	def build(self):
 		return Contenedor_01()
 		
