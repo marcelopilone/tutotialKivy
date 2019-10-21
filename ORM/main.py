edd5bd4a01a8c1e49cd02a4bb256a79b5d21a4ef
marcelopilone <mdpilone@gmail.com>
2019-10-21 19:17:45 -0300

    ORM ANDANDO

--- ORM/main.py
+++ ORM/main.py
@@ -1,4 +1,8 @@
 import kivy
+import sqlalchemy as db
+import json
+engine = db.create_engine('mysql+mysqlconnector://root:asdasdasdasd@localhost:3306/uejn_afigestion')
+
 kivy.require('1.9.0')
 
 from kivy.app import App
@@ -6,44 +10,38 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.lang import Builder
-
+from kivy.uix.floatlayout import FloatLayout
 
 from kivy.config import Config
 Config.set('graphics', 'width', 800)
 Config.set('graphics', 'height', 400)
 
 class Contenedor_01(BoxLayout):
-		def mostrarBoton(self,nroBoton):
-			if nroBoton == 1:
-				self.add_widget(Contenedor_MostrarTexto_Boton_Uno())
-			if nroBoton == 2:
-				self.add_widget(Contenedor_MostrarTexto_Boton_Dos())
-			if nroBoton == 3:
-				self.add_widget(Contenedor_MostrarTexto_Boton_Tres())
-			if nroBoton == 4:
-				self.add_widget(Contenedor_MostrarTexto_Boton_Cuatro())
+    def __init__(self, **kwargs):
+        super(Contenedor_01, self).__init__(**kwargs)
+        connection = engine.connect()
+        metadata = db.MetaData()
+        beneficios = db.Table('afiliacion_estados', metadata, autoload=True, autoload_with=engine)
+        #print(beneficios.columns.keys())
+        query = db.select([beneficios])
+        ResultProxy = connection.execute(query)
+        ResultSet = ResultProxy.fetchall()
+        def recorrerArray( ResultSet ):
+            for x in ResultSet[:5]:
+                if(x[2]):
+                    self.add_widget(Label(text=x[1]))
+                    print(x[2])
 
 
-class Titulo(BoxLayout):
-	None
+        print(recorrerArray( ResultSet ))
 
-class Contenedor_MostrarTexto_Boton_Uno(GridLayout):
-	None
 
-class Contenedor_MostrarTexto_Boton_Dos(BoxLayout):
-	None
+    
+class MainApp(App): 
+    title = "Hola Mundo"
 
-class Contenedor_MostrarTexto_Boton_Tres(BoxLayout):
-	None
-
-class Contenedor_MostrarTexto_Boton_Cuatro(BoxLayout):
-	None	
-	
-class MainApp(App):	
-	title = "Hola Mundo"
-
-	def build(self):
-		return Contenedor_01()
-		
+    def build(self):
+        return Contenedor_01()
+        
 if __name__ == '__main__':
-	MainApp().run()
+    MainApp().run()
